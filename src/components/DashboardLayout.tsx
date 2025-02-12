9b35221dc78f6f4102ebf45b568da5c6a442c6b1
tiagossm <eng.tiagosm@gmail.com>
2025-02-12 10:28:56 -0300

    Dashboardlayout.tsx tirar sidebar da tela inicial

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,18 +1,20 @@
-
 import { ReactNode } from "react";
 import { AppSidebar } from "./AppSidebar";
 import { SidebarProvider } from "./ui/sidebar";
+import { useAuth } from "@/components/ui/AuthContext"; // Importando autenticação
 
 interface DashboardLayoutProps {
   children: ReactNode;
 }
 
 function DashboardLayout({ children }: DashboardLayoutProps) {
+  const { isAuthenticated } = useAuth(); // Verificando se o usuário está autenticado
+
   return (
     <SidebarProvider>
       <div className="min-h-screen flex w-full bg-gray-900">
-        <AppSidebar />
-        <main className="flex-1 ml-20 lg:ml-64 p-8 transition-all duration-300">
+        {isAuthenticated && <AppSidebar />} {/* Sidebar só aparece após o login */}
+        <main className={`flex-1 ${isAuthenticated ? 'ml-20 lg:ml-64' : ''} p-8 transition-all duration-300`}>
           {children}
         </main>
       </div>
