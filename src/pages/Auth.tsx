9a3fa327b9b078bd6063cd3a85cb2fe937836aad
tiagossm <eng.tiagosm@gmail.com>
2025-03-02 20:30:37 -0300

    auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,4 +1,3 @@
-
 import { Shield } from "lucide-react";
 import { useAuthForm } from "@/hooks/useAuthForm";
 import { SessionChecker } from "@/components/auth/SessionChecker";
@@ -14,12 +13,11 @@ const Auth = () => {
     isSignUp,
     setIsSignUp,
     handleAuth,
-    handleUserRedirect
   } = useAuthForm();
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
-      <SessionChecker handleUserRedirect={handleUserRedirect} />
+      <SessionChecker />
       
       <div className="max-w-md w-full space-y-8">
         <div className="flex flex-col items-center">
