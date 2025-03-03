9fc333bfbec21bc155d748b68cafb4afe90ff313
tiagossm <eng.tiagosm@gmail.com>
2025-03-02 21:54:51 -0300

    s

--- src/components/auth/SessionChecker.tsx
+++ src/components/auth/SessionChecker.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/integrations/supabase/client";
 import { useNavigate } from "react-router-dom";
 
 export const SessionChecker = () => {
