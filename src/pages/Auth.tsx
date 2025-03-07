e2bfe17983185dd2868e4699bc9761696cd47637
tiagossm <eng.tiagosm@gmail.com>
2025-03-06 23:37:21 -0300

    logo no meio

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -63,12 +63,12 @@ const Auth = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        {/* Exibir o logo 4x maior */}
-        <div className="flex justify-center">
+        {/* Exibir o logo 4x maior com margem superior de 1cm */}
+        <div className="flex justify-center" style={{ marginTop: "1cm" }}>
           <img
             src="/lovable-uploads/logobrancoFT.png"
             alt="Logo"
-            className="h-64 w-auto"  // Aumentado de h-16 para h-64
+            className="h-64 w-auto"
           />
         </div>
         <div className="flex flex-col items-center">
