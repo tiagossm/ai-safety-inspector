92eab25d098421dcd1892944135639fd250f1ff7
tiagossm <eng.tiagosm@gmail.com>
2025-03-11 16:21:25 -0300

    API

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -6,7 +6,11 @@ import type { Database } from './types';
 let supabaseInstance: ReturnType<typeof createClient<Database>> | null = null;
 
 const SUPABASE_URL = "https://jkgmgjjtslkozhehwmng.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprZ21namp0c2xrb3poZWh3bW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxNzc0MjYsImV4cCI6MjA1Mzc1MzQyNn0.KVx6pqvHJZ0m-tnI-M_oaBhRoxva0PJRYcaRorKWzEA";
+<<<<<<< HEAD
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprZ21namp0c2xrb3poZWh3bW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MDQ5MTUsImV4cCI6MjA1NzI4MDkxNX0.4WiEt8e-j4LHUl7PHda6xvb3mXSU1xiDjZxdrqm5TqU";
+=======
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprZ21namp0c2xrb3poZWh3bW5nIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTcwNDkxNSwiZXhwIjoyMDU3MjgwOTE1fQ.NaWBX7qlpKAdZPqQ2hTfvjLXFhGNADXbYP5-QhkasSo";
+>>>>>>> f58c3ae7f17570ff321b7998afafada0896ad29c
 
 function createSupabaseClient() {
   // Only create the client once
